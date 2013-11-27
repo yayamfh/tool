c2f11f2b1d536a54570e23345da7e47eb81e027b
yayazulkeffli <nurhidayah.zulkeffli@octo.com.my>
2013-11-27 16:18:40 +0800

    added

--- server/publications.js
+++ server/publications.js
@@ -9,3 +9,4 @@ Meteor.publish('notifications', function() {  return Notifications.find(); });
 
 Meteor.publish('newPosts', function(limit) {  return Posts.find({}, {sort: {submitted: -1}, limit: limit}); });
 Meteor.publish('singlePost', function(id) {  return id && Posts.find(id); });
+Meteor.publish('bestPosts', function(limit) {  return Posts.find({}, {sort: {votes: -1, submitted: -1}, limit: limit}); });
