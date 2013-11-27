8472123cc06d88cae807e2e3fd2b151e66934e68
yayazulkeffli <nurhidayah.zulkeffli@octo.com.my>
2013-11-27 11:54:19 +0800

    neww add

--- collections/posts.js
+++ collections/posts.js
@@ -30,4 +30,20 @@ Meteor.methods
     		submitted: new Date().getTime()   
     	 });
     var postId = Posts.insert(post);
-    return postId;  } });
+    return postId;  } });//
+
+Posts.allow({  
+	update: ownsDocument,  
+	remove: ownsDocument });
+Posts.deny({  
+update: function(userId, post, fieldNames) {
+    // may only edit the following two fields:    
+    return (_.without(fieldNames, 'url', 'title').length > 0);  } })
+
+
+// pick out the whitelisted keys 
+var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), 
+	{  userId: user._id,   author: user.username,   
+		submitted: new Date().getTime(),  
+		commentsCount: 0 });
+var postId = Posts.insert(post);
