09ff1e7e0d2817538246d5e7ebc73aa6b450c2a4
yayazulkeffli <nurhidayah.zulkeffli@octo.com.my>
2013-11-27 12:16:59 +0800

    NA

--- server/publications.js
+++ server/publications.js
@@ -1,1 +1,11 @@
+//Meteor.publish('posts', function() {  return Posts.find(); });
+//Meteor.publish('comments', function() {  return Comments.find(); });
+
+
 Meteor.publish('posts', function() {  return Posts.find(); });
+Meteor.publish('comments', function(postId) {  return Comments.find({postId: postId}); });
+
+Meteor.publish('notifications', function() {  return Notifications.find(); });
+
+Meteor.publish('newPosts', function(limit) {  return Posts.find({}, {sort: {submitted: -1}, limit: limit}); });
+Meteor.publish('singlePost', function(id) {  return id && Posts.find(id); });
