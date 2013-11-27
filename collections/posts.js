f053ac2c6e165c79dd56bcd7a5b7eb63394c88c0
yayazulkeffli <nurhidayah.zulkeffli@octo.com.my>
2013-11-27 16:16:59 +0800

    update

--- collections/posts.js
+++ collections/posts.js
@@ -23,14 +23,14 @@ Meteor.methods
     		postWithSameLink._id);    
 		}
     // pick out the whitelisted keys    
-    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), 
-    	{     
-    		userId: user._id,       
-    		author: user.username,       
-    		submitted: new Date().getTime()   
-    	 });
-    var postId = Posts.insert(post);
-    return postId;  } });//
+    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {  
+    	userId: user._id,   
+    	author: user.username,   
+    	submitted: new Date().getTime(),  
+    	commentsCount: 0,  upvoters: [],   votes: 0 
+    });
+var postId = Posts.insert(post);
+return postId;
 
 Posts.allow({  
 	update: ownsDocument,  
@@ -38,12 +38,31 @@ Posts.allow({
 Posts.deny({  
 update: function(userId, post, fieldNames) {
     // may only edit the following two fields:    
-    return (_.without(fieldNames, 'url', 'title').length > 0);  } })
+    return (_.without(fieldNames, 'url', 'title').length > 0);  } });
 
 
-// pick out the whitelisted keys 
-var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), 
-	{  userId: user._id,   author: user.username,   
-		submitted: new Date().getTime(),  
-		commentsCount: 0 });
-var postId = Posts.insert(post);
+ upvote: function(postId) {    
+ var user = Meteor.user();    
+ // ensure the user is logged in    
+ if (!user)      
+ 	throw new Meteor.Error(401, "You need to login to upvote");
+  Posts.update({      _
+  	id: postId,       
+  	upvoters: {$ne: user._id}    
+  }, {      
+  	$addToSet: {upvoters: user._id},      
+  	$inc: {votes: 1}   
+  }); 
+   } };
+ 
+
+    var post = Posts.findOne(postId);    
+    if (!post)      
+    	throw new Meteor.Error(422, 'Post not found');
+    if (_.include(post.upvoters, user._id))      
+    	throw new Meteor.Error(422, 'Already upvoted this post');
+    Posts.update(post._id, {      
+    	$addToSet: {upvoters: user._id},     
+    	 $inc: {votes: 1}   
+    	  });  
+});
